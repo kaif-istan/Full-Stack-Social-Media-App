f2e83c0a7f0a8acfa26d0c2f7160de4d200212f8
Md Kaif <kaif85077@gmail.com>
2025-09-17 22:50:39 +0530

    Sidebar and story feature created.

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
+import { dummyPostsData } from "../assets/assets";
+import Loading from "../components/Loading";
+import StoriesBar from "../components/StoriesBar";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [feeds, setFeeds] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchFeeds = async () => {
+    setFeeds(dummyPostsData);
+  };
+  useEffect(() => {
+    fetchFeeds();
+  }, []);
+  return !loading ? (
+    <div className="h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8">
+      {/* Stories and Post list */}
+      <div>
+        <StoriesBar />
+        <div className="p-4 space-y-6">List of post</div>
+      </div>
+      {/* Right Sidebar */}
+      <div>
+        <div>
+          <h1>Sponsored</h1>
+        </div>
+        <h1>Recent messages</h1>
+      </div>
+    </div>
+  ) : (
+    <Loading />
+  );
+};
 
 export default Feed;
