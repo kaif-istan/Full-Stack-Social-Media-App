700bfaacea83e0c006cd1445672bd95b1d1388cc
Md Kaif <kaif85077@gmail.com>
2025-09-18 12:01:21 +0530

    Create story feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,21 +9,25 @@ import Profile from "./pages/Profile";
 import CreatePost from "./pages/CreatePost";
 import { useUser } from "@clerk/clerk-react";
 import Layout from "./pages/Layout";
+import { Toaster } from "react-hot-toast";
 const App = () => {
   const { user } = useUser();
   return (
-    <Routes>
-      <Route path="/" element={!user ? <Login /> : <Layout />}>
-        <Route index element={<Feed />} />
-        <Route path="messages" element={<Messages />} />
-        <Route path="messages/:userId" element={<ChatBox />} />
-        <Route path="connections" element={<Connections />} />
-        <Route path="discover" element={<Discover />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="profile/:profileId" element={<Profile />} />
-        <Route path="create-post" element={<CreatePost />} />
-      </Route>
-    </Routes>
+    <>
+      <Toaster />
+      <Routes>
+        <Route path="/" element={!user ? <Login /> : <Layout />}>
+          <Route index element={<Feed />} />
+          <Route path="messages" element={<Messages />} />
+          <Route path="messages/:userId" element={<ChatBox />} />
+          <Route path="connections" element={<Connections />} />
+          <Route path="discover" element={<Discover />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="profile/:profileId" element={<Profile />} />
+          <Route path="create-post" element={<CreatePost />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
