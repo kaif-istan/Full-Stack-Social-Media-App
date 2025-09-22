775340d27935715c92961995a80b08018fe9b930
Md Kaif <kaif85077@gmail.com>
2025-09-22 14:39:52 +0530

    multer error fixed.

--- server/configs/multer.js
+++ server/configs/multer.js
@@ -1,18 +1,6 @@
 import multer from "multer";
-import path from "path";
-import fs from "fs";
 
-
-if (!fs.existsSync("uploads")) fs.mkdirSync("uploads");
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/"); // saves to uploads folder
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
+const storage = multer.diskStorage({});
 
 export const upload = multer({ storage });
 
